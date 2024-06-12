f02c1f1f5deab153ca829ca41ba05eb3be72c28a
Jonathan VIRGA <jonathan.virga@mfi.fr>
2024-06-12 09:40:45 +0200

    Use `palette` crate to handle color, add fade duration argument.

--- examples/test.rs
+++ examples/test.rs
@@ -1,10 +1,13 @@
 extern crate blink1;
 
 use blink1::Blink1Device;
-use std::{thread, time};
+use palette::rgb::Rgb;
+use std::str::FromStr;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let ten_millis = time::Duration::from_millis(1000);
+    let sleep = Duration::from_secs(10);
 
     match Blink1Device::get_serials() {
         Ok(serials) => {
@@ -12,20 +15,22 @@ fn main() {
                 println!("Serial number: {}", serial)
             }
         }
-        Err(e) => println!("{}", e)
+        Err(e) => println!("{}", e),
     }
 
     let device = Blink1Device::open_first().expect("Failed to open device.");
 
     println!("Fade to red...");
     device
-        .fade_to_rgb(0, 255, 0, 0)
+        .fade_to_rgb(
+            0,
+            Rgb::from_str("#ff0000").unwrap().into(),
+            Duration::from_secs(1),
+        )
         .expect("Failed to set color.");
 
-    thread::sleep(ten_millis);
+    thread::sleep(sleep);
 
     println!("Turn off...");
-    device
-        .fade_off(0)
-        .expect("Failed to set color.");
+    device.off(0).expect("Failed to set color.");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
-extern crate hidapi;
-
 use hidapi::HidApi;
 use hidapi::HidError;
 use hidapi::HidResult;
+use palette::rgb::Rgb;
+use palette::Srgb;
+use std::time::Duration;
 
 const BLINK1_VENDOR_ID: u16 = 0x27b8;
 const BLINK1_PRODUCT_ID: u16 = 0x01ed;
 
 const BLINK1_REPORT_ID: u8 = 1;
 const BLINK1_REPORT_SIZE: usize = 8;
-const BLINK1_BUF_SIZE:usize = BLINK1_REPORT_SIZE + 1;
+const BLINK1_BUF_SIZE: usize = BLINK1_REPORT_SIZE + 1;
 
 pub struct Blink1Device {
     device: hidapi::HidDevice,
@@ -37,7 +38,7 @@ impl Blink1Device {
                     Ok(device) => Ok(Blink1Device { device }),
                     Err(e) => Err(e),
                 }
-            },
+            }
             Err(e) => Err(e),
         }
     }
@@ -48,34 +49,46 @@ impl Blink1Device {
                 let mut vec: Vec<String> = Vec::new();
 
                 for device in api.device_list() {
-                    if device.product_id() == BLINK1_PRODUCT_ID && device.vendor_id() == BLINK1_VENDOR_ID {
+                    if device.product_id() == BLINK1_PRODUCT_ID
+                        && device.vendor_id() == BLINK1_VENDOR_ID
+                    {
                         if let Some(serial) = &device.serial_number() {
                             vec.push(serial.to_string());
                         }
                     }
                 }
                 Ok(vec)
-            },
+            }
             Err(e) => Err(e),
         }
     }
 
-    pub fn fade_off(&self, index: u8) -> HidResult<()> {
-        self.fade_to_rgb(index, 0, 0, 0)
+    pub fn off(&self, index: u8) -> HidResult<()> {
+        self.fade_off(index, Duration::from_secs(0))
     }
 
-    pub fn fade_to_rgb(&self, index: u8, r: u8, g: u8, b: u8) -> HidResult<()> {
-        let dms = 100; // 1000 ms
+    pub fn fade_off(&self, index: u8, fade_duration: Duration) -> HidResult<()> {
+        self.fade_to_rgb(index, Rgb::new(0., 0., 0.), fade_duration)
+    }
+
+    pub fn rgb(&self, index: u8, color: Rgb) -> HidResult<()> {
+        self.fade_to_rgb(index, color, Duration::from_secs(0))
+    }
+
+    pub fn fade_to_rgb(&self, index: u8, color: Rgb, fade_duration: Duration) -> HidResult<()> {
+        let color: Srgb<u8> = color.into();
+        let th = ((fade_duration.as_millis() as f32 / 10.).round() as u32) >> 8;
+        let tl = ((fade_duration.as_millis() as f32 / 10.).round() as u32) & 0xff;
 
         let mut buf: [u8; BLINK1_BUF_SIZE] = [0; BLINK1_BUF_SIZE];
 
         buf[0] = BLINK1_REPORT_ID;
-        buf[1] = b'c';   // command code for 'fade to rgb'
-        buf[2] = r;
-        buf[3] = g;
-        buf[4] = b;
-        buf[5] = 0;
-        buf[6] = dms % 0xff;
+        buf[1] = b'c'; // command code for 'fade to rgb'
+        buf[2] = color.red;
+        buf[3] = color.green;
+        buf[4] = color.blue;
+        buf[5] = th as u8;
+        buf[6] = tl as u8;
         buf[7] = index;
 
         self.device.send_feature_report(&buf)
